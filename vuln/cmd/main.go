a98d64beaf417ab2d96a14771038cb15f32eaf8a
Bb V <butbeatifulv@yahoo.com>
2026-02-14 13:21:27 +0300

    Multistaging + example config

--- vuln/cmd/main.go
+++ vuln/cmd/main.go
@@ -1,55 +1,56 @@
 package main
 
 import (
-    "vuln/internal/components"
-    "vuln/internal/config"
-    "context"
-    "fmt"
-    "golang.org/x/sync/errgroup"
-    "log"
-    "log/slog"
-    "os"
-    "os/signal"
-    "syscall"
+	"context"
+	"fmt"
+	"log"
+	"log/slog"
+	"os"
+	"os/signal"
+	"syscall"
+	"vuln/internal/components"
+	"vuln/internal/config"
+
+	"golang.org/x/sync/errgroup"
 )
 
 func main() {
-    cfg, err := config.LoadConfig()
-    if err != nil {
-        log.Println(err.Error())
-        os.Exit(1)
-    }
+	cfg, err := config.LoadConfig()
+	if err != nil {
+		log.Println(err.Error())
+		os.Exit(1)
+	}
 
-    logger := components.SetupLogger(cfg.Env)
+	logger := components.SetupLogger(cfg.Env)
 
-    components, err := components.InitComponents(cfg, logger)
-    if err != nil {
-        logger.Error("bad configuration", slog.String("error", err.Error()))
-        os.Exit(1)
-    }
-    defer components.Shutdown()
+	components, err := components.InitComponents(cfg, logger)
+	if err != nil {
+		logger.Error("bad configuration", slog.String("error", err.Error()))
+		os.Exit(1)
+	}
+	defer components.Shutdown()
 
-    eg, ctx := errgroup.WithContext(context.Background())
+	eg, ctx := errgroup.WithContext(context.Background())
 
-    sigQuit := make(chan os.Signal, 1)
-    signal.Notify(sigQuit, syscall.SIGINT, syscall.SIGTERM)
+	sigQuit := make(chan os.Signal, 1)
+	signal.Notify(sigQuit, syscall.SIGINT, syscall.SIGTERM)
 
-    eg.Go(func() error {
-        logger.Info("Scraper started")
-        return components.Scraper.Run(ctx)
-    })
+	eg.Go(func() error {
+		logger.Info("Scraper started")
+		return components.Scraper.Run(ctx)
+	})
 
-    eg.Go(func() error {
-        select {
-        case <-ctx.Done():
-            return ctx.Err()
+	eg.Go(func() error {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
 
-        case s := <-sigQuit:
-            logger.Info("Captured signal", slog.String("signal", s.String()))
-            return fmt.Errorf("captured signal: %v", s)
-        }
-    })
+		case s := <-sigQuit:
+			logger.Info("Captured signal", slog.String("signal", s.String()))
+			return fmt.Errorf("captured signal: %v", s)
+		}
+	})
 
-    err = eg.Wait()
-    logger.Info("Gracefully shutting down", slog.String("error", err.Error()))
+	err = eg.Wait()
+	logger.Info("Gracefully shutting down", slog.String("error", err.Error()))
 }
